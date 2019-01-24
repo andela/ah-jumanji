8ad33bff3e6330d40a8620d32bddc479ce427a0c
GransonO <oyombegranson@gmail.com>
2019-01-24 22:24:21 +0300

    fix(image upoad): Upload article image to cloudinary - uploads image when user add it to the editor - show upload progress - parse and return image data as required by editor [Finishes #163463708]

--- authors/apps/articles/urls.py
+++ authors/apps/articles/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
-from .views import ArticleView, ArticleSpecificFunctions
+from .views import ArticleView, ArticleSpecificFunctions, ImageUpload
 
 urlpatterns = [
     path('articles/', ArticleView.as_view(),
          name='articles'),
+    path('articles/image/upload', ImageUpload.as_view(),
+         name='articles_image'),
     path('articles/<slug>/', ArticleSpecificFunctions.as_view(),
          name='articleSpecific'),
     path('q/', include('authors.apps.search.urls'),
