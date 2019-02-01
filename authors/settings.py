c8afb0577b0e3ade1021e78fdd4153a25ffad05f
Francis Masha <francismasha96@gmail.com>
2019-02-01 08:48:15 +0300

    fix(pagination): Increase number of pages for pagination [Starts #163644676]

--- authors/settings.py
+++ authors/settings.py
@@ -200,7 +200,7 @@ REST_FRAMEWORK = {
 
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5,
+    'PAGE_SIZE': 12,
 
     # Priority list of parsers for swagger document
     'DEFAULT_PARSER_CLASSES': [
